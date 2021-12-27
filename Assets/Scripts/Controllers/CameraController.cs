0993cbf3381b77198a7740f7285a04178cde49f6
Henry Moss <henry@flip.fitness>
2021-12-27 14:17:34 -0500

    Updated radio room layout and implemented a dynamic zoom camera

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -8,9 +8,15 @@ public class CameraController : MonoBehaviour
 
     public Transform target;
     public float smoothTime = 0.3F;
-    public float camXOffset;
-    public float camYOffset;
-    public float camZOffset;
+    [SerializeField] float camXOffset;
+    [SerializeField] float camYOffset;
+    [SerializeField] float camZOffset;
+    Quaternion baseRot;
+
+    [SerializeField] bool focus;
+    [SerializeField] float camFocusSize;
+    [SerializeField] float camDefaultSize;
+    [SerializeField] float focusRate;
 
 
     private void Awake()
@@ -24,21 +30,49 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 cameraPos = Camera.main.transform.position;
+        camXOffset = cameraPos.x;
+        camYOffset = cameraPos.y;
+        camZOffset = cameraPos.z;
+
+        baseRot = transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 pos = target.transform.position;
-        pos.x += camXOffset;
-        pos.y += camYOffset;
-        pos.z += camZOffset;
-        transform.position = pos;
+        if (!focus)
+        {
+            pos.x += camXOffset;
+            pos.y += camYOffset;
+            pos.z += camZOffset;
+        }
+
+        Vector3 newPos = Vector3.Lerp(transform.position, pos, smoothTime);
+        transform.position = newPos;
+
+        if (focus)
+        {
+            Vector3 newRot = Vector3.RotateTowards(transform.position, target.position, smoothTime * Time.deltaTime, 0.0f);
+            transform.rotation = target.localRotation;
+
+            if (Camera.main.orthographicSize > camFocusSize)
+                Camera.main.orthographicSize -= focusRate * Time.deltaTime; //= Mathf.Lerp(camDefaultSize, camFocusSize, Time.deltaTime);//0.5f;
+        }
+        else
+        {
+            transform.rotation = baseRot;
+
+            if (Camera.main.orthographicSize < camDefaultSize)
+                Camera.main.orthographicSize += focusRate * Time.deltaTime; //= Mathf.Lerp(camFocusSize, camDefaultSize, Time.deltaTime); //Camera.main.orthographicSize = 4f;
+        }
+
+
     }
 
     public void SetTarget(GameObject newTargetObj)
     {
         target = newTargetObj.transform;
-    } 
+    }
 }
