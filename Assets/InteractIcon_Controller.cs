b7a9ba1c014b909058c79c6f49601fecb47c9087
Henry Moss <henry@flip.fitness>
2022-01-03 20:35:26 -0500

    Fixed an issue where the interacticon_controller script was being disabled whent eh user was not within range of an interactable object

--- Assets/InteractIcon_Controller.cs
+++ Assets/InteractIcon_Controller.cs
@@ -46,6 +46,6 @@ public class InteractIcon_Controller : MonoBehaviour
         else
             isActive = false;
 
-        this.gameObject.SetActive(isActive);
+        icon.enabled = isActive;
     }
 }
