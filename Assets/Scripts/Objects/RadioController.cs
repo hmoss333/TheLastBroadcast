58e07497d6922f9a57e4c318c662cabeb5500e5c
Henry Moss <henry@flip.fitness>
2022-02-11 00:07:17 -0500

    Updated the radio controller to have a preset system; incorporated standard radio tuning to dynamically allow the player to scan from all presets

--- Assets/Scripts/Objects/RadioController.cs
+++ Assets/Scripts/Objects/RadioController.cs
@@ -14,13 +14,15 @@ public class RadioController : InteractObject
     [SerializeField] GameObject focusPoint;
     [SerializeField] AudioSource staticSource;
 
-    [SerializeField] float currentFrequency;
-    [SerializeField] float maxFrequency;
+    [SerializeField][Range(0f, 10f)] float currentFrequency;
     [SerializeField] TextMeshPro frequencyText;
 
     [SerializeField] GameObject dialObj;
     [SerializeField] float rotSpeed;
     private float xInput;
+    [SerializeField] float[] stationNums;
+    //[SerializeField] float activeStation;
+    int stationIndex;
 
 
     private void Awake()
@@ -29,6 +31,9 @@ public class RadioController : InteractObject
             instance = this;
         else
             Destroy(this.gameObject);
+
+
+        stationIndex = 0;
     }
 
     private void Update()
@@ -42,10 +47,10 @@ public class RadioController : InteractObject
             tempSpeed = 0.0f;
             currentFrequency = 0.0f;
         }
-        else if (currentFrequency > maxFrequency)
+        else if (currentFrequency > 10f)
         {
             tempSpeed = 0.0f;
-            currentFrequency = maxFrequency;
+            currentFrequency = 10f;
         }
         else
         {
@@ -55,9 +60,33 @@ public class RadioController : InteractObject
 
         if (interacting)
         {
-            xInput = Input.GetAxis("Horizontal");
-            dialObj.transform.Rotate(0.0f, xInput * tempSpeed, 0.0f);
-            currentFrequency += (float)(xInput * Time.deltaTime);
+            if (Input.GetKey("right shift"))
+            {
+                if (Input.GetKeyDown("left"))
+                {
+                    stationIndex--;
+                    if (stationIndex < 0)
+                        stationIndex = stationNums.Length - 1;
+
+                    dialObj.transform.Rotate(0.0f, dialObj.transform.localEulerAngles.y - 25f, 0.0f);
+                }
+                if (Input.GetKeyDown("right"))
+                {
+                    stationIndex++;
+                    if (stationIndex > stationNums.Length - 1)
+                        stationIndex = 0;
+
+                    dialObj.transform.Rotate(0.0f, dialObj.transform.localEulerAngles.y + 25f, 0.0f);
+                }
+
+                currentFrequency = stationNums[stationIndex];
+            }
+            else
+            {
+                xInput = Input.GetAxis("Horizontal");
+                dialObj.transform.Rotate(0.0f, xInput * tempSpeed, 0.0f);
+                currentFrequency += (float)(xInput * Time.deltaTime);
+            }
 
             float displayFrequency = currentFrequency * 10f;
             frequencyText.text = displayFrequency.ToString("F1");
