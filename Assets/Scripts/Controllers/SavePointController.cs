ea5d8b8d54b7929225db92ccd90a1928d5b41f45
Henry Moss <henry@flip.fitness>
2022-02-23 07:31:53 -0500

    Updated save point logic to cycle through levels more cleanly

--- Assets/Scripts/Controllers/SavePointController.cs
+++ Assets/Scripts/Controllers/SavePointController.cs
@@ -10,6 +10,7 @@ public class SavePointController : InteractObject
     public Transform initPoint;
     [SerializeField] GameObject focusPoint;
     //[SerializeField] TextMeshPro saveText;
+    [SerializeField] int sceneVal;
     [SerializeField] string sceneToLoad;
 
     //TODO add logic here to allow the player to select if they would like to return to the Facility
@@ -24,46 +25,36 @@ public class SavePointController : InteractObject
         {
             if (Input.GetKeyDown("left"))
             {
-                sceneToLoad = NextSceneName(-1);
+                sceneVal--;
+                CycleSceneName();
             }
             else if (Input.GetKeyDown("right"))
             {
-                sceneToLoad = NextSceneName(1);
+                sceneVal++;
+                CycleSceneName();
             }
-            //else if (Input.GetKeyDown("space"))
-            //{
-            //    if (sceneToLoad == SaveDataController.instance.saveData.currentScene)
-            //        SaveGame();
-            //    else
-            //        SceneManager.LoadScene(sceneToLoad);
-            //}
+            else if (Input.GetKeyDown("up") && sceneToLoad != SaveDataController.instance.saveData.currentScene)
+            {
+                PlayerController.instance.ToggleAvatar();
+                SceneManager.LoadScene(sceneToLoad);
+            }
         }
     }
 
 
-    string NextSceneName(int incrementNum)
+    void CycleSceneName()
     {
-        int levelNum = 0;
-        string returnLevelName = "";
+        List<string> sceneNames = new List<string>();
         for (int i = 0; i < SaveDataController.instance.saveData.scenarios.Count; i++)
         {
-            if (SaveDataController.instance.saveData.scenarios[i].sceneName == SaveDataController.instance.saveData.currentScene)
-            {
-                levelNum = i + incrementNum;
-                returnLevelName = SaveDataController.instance.saveData.scenarios[levelNum].sceneName;
-                break;
-            }
+            sceneNames.Add(SaveDataController.instance.saveData.scenarios[i].sceneName);
         }
 
-        return returnLevelName;
-    }
-
-    void SaveGame()
-    {
-        Debug.Log("Saving game");
-        SceneInitController.instance.SaveInteractObjs();
-        SaveDataController.instance.SetSavePoint(SceneManager.GetActiveScene().name, ID);
-        SaveDataController.instance.SaveFile();
+        if (sceneVal > sceneNames.Count - 1)
+            sceneVal = 0;
+        else if (sceneVal < 0)
+            sceneVal = sceneNames.Count - 1;
+        sceneToLoad = sceneNames[sceneVal];
     }
 
     public override void Interact()
@@ -74,23 +65,12 @@ public class SavePointController : InteractObject
         CameraController.instance.SetTarget(interacting ? focusPoint : PlayerController.instance.gameObject);
         CameraController.instance.FocusTarget();
 
-        //if (interacting)
-        //{
-        //Debug.Log("Saving game");
-        //SceneInitController.instance.SaveInteractObjs();
-        //SaveDataController.instance.SetSavePoint(SceneManager.GetActiveScene().name, ID);
-        //SaveDataController.instance.SaveFile();
-        //{
-
-        if (Input.GetKeyDown("space"))
+        if (interacting)
         {
-            if (sceneToLoad == SaveDataController.instance.saveData.currentScene)
-                SaveGame();
-            else
-            {
-                SceneManager.LoadScene(sceneToLoad);
-                PlayerController.instance.ToggleAvatar();
-            }
+            Debug.Log("Saving game");
+            SceneInitController.instance.SaveInteractObjs();
+            SaveDataController.instance.SetSavePoint(SceneManager.GetActiveScene().name, ID);
+            SaveDataController.instance.SaveFile();
         }
     }
 }
