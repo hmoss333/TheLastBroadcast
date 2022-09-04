cc59ad630eb5bb6c95d464863d794a2204a8c5ca
Henry Moss <henry@flip.fitness>
2022-09-04 18:54:33 -0400

    Splitting commit part 5

---
+++ Assets/Imported/Interacts/Radio Equipment/Ham_Transciever/Scripts/_Display_Anim_HRT.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Display_Anim_HRT : MonoBehaviour {
+
+	public GameObject Display_01;
+	public GameObject Display_02;
+	public GameObject Display_03;
+	public GameObject Display_04;
+	public GameObject Display_05;
+
+	void Start () {
+
+		StartCoroutine(Disp());
+		Display_01.GetComponent<MeshRenderer> ().enabled = false;
+		Display_02.GetComponent<MeshRenderer> ().enabled = false;
+		Display_03.GetComponent<MeshRenderer> ().enabled = false;
+		Display_04.GetComponent<MeshRenderer> ().enabled = false;
+		Display_05.GetComponent<MeshRenderer> ().enabled = false;
+
+	}
+
+	IEnumerator Disp()
+	{
+		while(true)
+		{
+			Display_01.GetComponent<MeshRenderer> ().enabled = true;
+			Display_02.GetComponent<MeshRenderer> ().enabled = false;
+			Display_03.GetComponent<MeshRenderer> ().enabled = false;
+			Display_04.GetComponent<MeshRenderer> ().enabled = false;
+			Display_05.GetComponent<MeshRenderer> ().enabled = false;
+			yield return new WaitForSeconds(1);
+			Display_01.GetComponent<MeshRenderer> ().enabled = false;
+			Display_02.GetComponent<MeshRenderer> ().enabled = true;
+			Display_03.GetComponent<MeshRenderer> ().enabled = false;
+			Display_04.GetComponent<MeshRenderer> ().enabled = false;
+			Display_05.GetComponent<MeshRenderer> ().enabled = false;
+			yield return new WaitForSeconds(1);
+			Display_01.GetComponent<MeshRenderer> ().enabled = false;
+			Display_02.GetComponent<MeshRenderer> ().enabled = false;
+			Display_03.GetComponent<MeshRenderer> ().enabled = true;
+			Display_04.GetComponent<MeshRenderer> ().enabled = false;
+			Display_05.GetComponent<MeshRenderer> ().enabled = false;
+			yield return new WaitForSeconds(0.25f);
+			Display_01.GetComponent<MeshRenderer> ().enabled = false;
+			Display_02.GetComponent<MeshRenderer> ().enabled = false;
+			Display_03.GetComponent<MeshRenderer> ().enabled = false;
+			Display_04.GetComponent<MeshRenderer> ().enabled = true;
+			Display_05.GetComponent<MeshRenderer> ().enabled = false;
+			yield return new WaitForSeconds(1);
+			Display_01.GetComponent<MeshRenderer> ().enabled = false;
+			Display_02.GetComponent<MeshRenderer> ().enabled = false;
+			Display_03.GetComponent<MeshRenderer> ().enabled = false;
+			Display_04.GetComponent<MeshRenderer> ().enabled = false;
+			Display_05.GetComponent<MeshRenderer> ().enabled = true;
+			yield return new WaitForSeconds(0.05f);
+
+			yield return null;
+	}
+}
+}
---
+++ Assets/Imported/Interacts/Radio Equipment/VFO/Scripts/_Lamp_1_Anim_VFO.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Lamp_1_Anim_VFO : MonoBehaviour {
+
+	public GameObject Lamp;
+
+	void Start () {
+
+		StartCoroutine(Disp());
+		Lamp.GetComponent<MeshRenderer> ().enabled = false;
+	}
+
+	IEnumerator Disp()
+	{
+		while(true)
+		{
+			Lamp.GetComponent<MeshRenderer> ().enabled = true;
+			yield return new WaitForSeconds(1.0f);
+			Lamp.GetComponent<MeshRenderer> ().enabled = false;
+			yield return new WaitForSeconds(1.0f);
+			yield return null;
+	}
+}
+}
