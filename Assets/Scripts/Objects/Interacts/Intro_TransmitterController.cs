988969b28b7732a0e8c1af9eef0e2bab9873e216
Henry Moss <henry@flip.fitness>
2022-09-24 00:25:26 -0400

    Updated the forest scene to include transmitor to teleport player to the morgue/facility level; added textures and more lighting; adjusted the brick wall material to be aligned properly based on level geometry; created a new script for the intro transmitter so that it will not give an ability

---
+++ Assets/Scripts/Objects/Interacts/Intro_TransmitterController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Intro_TransmitterController : InteractObject
+{
+    [SerializeField] GameObject focusPoint;
+    [SerializeField] string nextScene;
+
+
+    public override void Interact()
+    {
+        if (active)
+        {
+            base.Interact();
+
+            PlayerController.instance.ToggleAvatar();
+            CameraController.instance.SetTarget(interacting ? focusPoint : PlayerController.instance.gameObject);
+            CameraController.instance.FocusTarget();
+            active = false;
+
+            //if (!hasActivated)
+            //{
+            //    //Debug.Log($"Actived transmitter for {sceneToActivate} station");
+            //    hasActivated = true;
+            //    active = false;
+            //    SaveDataController.instance.SaveObjectData(SceneManager.GetActiveScene().name); //Save object states
+            //    SaveDataController.instance.SaveFile();
+            //}
+
+            StartCoroutine(LoadRoom(nextScene));
+        }
+    }
+
+    IEnumerator LoadRoom(string sceneToLoad)
+    {
+        yield return new WaitForSeconds(1f);
+
+        CamEffectController.instance.SetEffectValues(true);
+
+        yield return new WaitForSeconds(2.5f); //change this to wait while any dialogue is playing
+
+        FadeController.instance.StartFade(1.0f, 7.5f);
+
+        while (FadeController.instance.isFading)
+            yield return null;
+
+        CamEffectController.instance.SetEffectValues(false);
+        SaveDataController.instance.SetSavePoint(sceneToLoad, 0);
+        PlayerController.instance.ToggleAvatar();
+        SceneManager.LoadSceneAsync(sceneToLoad);
+    }
+}
