12a428b9ca5a05ffc3456ad5cb49364d6262693c
Henry Moss <henry@flip.fitness>
2023-03-02 19:15:12 -0600

    Updated generator prefab to be active before the transciever and/or antenna are activated; added dialogue popup when successfully interacting with the generator

--- Assets/Scripts/Objects/Equipment/GeneratorController.cs
+++ Assets/Scripts/Objects/Equipment/GeneratorController.cs
@@ -4,26 +4,56 @@ using UnityEngine;
 
 public class GeneratorController : InteractObject
 {
+    [Header("Equipment References")]
+    [SerializeField] TranscieverController transciever;
+    [SerializeField] AntennaController antenna;
+
     public override void Interact()
     {
         if (active && !hasActivated)
         {
             base.Interact();
 
-            PlayerController.instance.ToggleAvatar();
-            CameraController.instance.SetTarget(interacting ? focusPoint : PlayerController.instance.gameObject);
-            CameraController.instance.FocusTarget();
+            //PlayerController.instance.ToggleAvatar();
+            //CameraController.instance.SetTarget(interacting ? focusPoint : PlayerController.instance.gameObject);
+            //CameraController.instance.FocusTarget();
+
+            //transciever.Activate();
+            //antenna.Activate();
+
+            //UIController.instance.SetDialogueText("Power has been restored");
+            //UIController.instance.ToggleDialogueUI(true);
         }
 
         //staticSource.mute = !interacting;
+    }
+
+    public override void StartInteract()
+    {
+        //base.StartInteract();
+        PlayerController.instance.ToggleAvatar();
+        CameraController.instance.SetTarget(interacting ? focusPoint : PlayerController.instance.gameObject);
+        CameraController.instance.FocusTarget();
+
+        transciever.Activate();
+        antenna.Activate();
+
+        UIController.instance.SetDialogueText("Power has been restored");
+        UIController.instance.ToggleDialogueUI(true);
     }
 
     public override void EndInteract()
     {
         //if (activated)
         //{
+        PlayerController.instance.ToggleAvatar();
+        CameraController.instance.SetTarget(interacting ? focusPoint : PlayerController.instance.gameObject);
+        CameraController.instance.FocusTarget();
+
         hasActivated = true;
         SaveDataController.instance.SaveObjectData(SaveDataController.instance.saveData.currentScene);
+
+        UIController.instance.ToggleDialogueUI(false);
         //}
     }
 }
