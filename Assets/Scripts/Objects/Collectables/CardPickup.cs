7d73be47848170477144f1726dbe29a069d28b18
Henry Moss <henry@flip.fitness>
2023-03-07 11:14:53 -0500

    Fixed an issue where collecting lower level security cards would overwrite higher levels; cards will now check your current level and only update if the new level is higher than the current; updated demo level with test objects

--- Assets/Scripts/Objects/Collectables/CardPickup.cs
+++ Assets/Scripts/Objects/Collectables/CardPickup.cs
@@ -6,26 +6,28 @@ using static AbilityPickup;
 public class CardPickup : InteractObject
 {
     [SerializeField] int cardLevel;
-    [SerializeField] string cardText;
     [SerializeField] Sprite cardIcon;
 
     private void Start()
     {
-        gameObject.SetActive(!hasActivated);
+        gameObject.SetActive(!hasActivated);      
     }
 
     public override void StartInteract()
     {
-        UIController.instance.ToggleAbilityUI(cardText, cardIcon);
-        SaveDataController.instance.SetSecurityCardLevel(cardLevel);
-        SaveDataController.instance.SaveFile();
+        UIController.instance.ToggleAbilityUI($"Collected Security Card Level {cardLevel}", cardIcon);
+        if (SaveDataController.instance.GetSecurityCardLevel() < cardLevel)
+        {
+            SaveDataController.instance.SetSecurityCardLevel(cardLevel);
+            SaveDataController.instance.SaveFile();
+        }
         print($"Collected Security Card Level {cardLevel}");
     }
 
     public override void EndInteract()
     {
         SetHasActivated();
-        UIController.instance.ToggleAbilityUI(cardText, cardIcon);
+        UIController.instance.ToggleAbilityUI("", cardIcon);
         gameObject.SetActive(false);
     }
 }
