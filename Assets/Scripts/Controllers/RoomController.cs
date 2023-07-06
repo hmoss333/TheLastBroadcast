6a4030e813d91328b3466718467e0195a9745684
Henry Moss <henry@flip.fitness>
2023-07-06 14:47:51 -0400

    Added new font assets; created a new Break Room; adjusted several lights; moved some materials into folders for better organization

--- Assets/Scripts/Controllers/RoomController.cs
+++ Assets/Scripts/Controllers/RoomController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RoomController : MonoBehaviour
 {
-    AudioSource audioSource;
+    [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip backgroundAudioClip;
 
 
