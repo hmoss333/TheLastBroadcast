c754d62699f8e56dc48ee3cfa904e9e8c1f506c3
Henry Moss <henry@flip.fitness>
2023-07-19 00:55:54 -0400

    Added more variations to footstep audio clips; updated the Footstep controller to use a raycast to check for specific layers and change the current clip based on the ground; added more layer variants for floor types; adjusted the animation event timing for the walk animation

--- Assets/Scripts/Controllers/Character/FootstepController.cs
+++ Assets/Scripts/Controllers/Character/FootstepController.cs
@@ -1,15 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class FootstepController : MonoBehaviour
 {
+    [Header("Footstep Types")]
+    [SerializeField] AudioClip concreteClip;
+    [SerializeField] AudioClip glassClip;
+    [SerializeField] AudioClip metalClip;
+    [SerializeField] AudioClip grassClip;
+
+    [Header("Sources")]
+    [SerializeField] private LayerMask groundLayer;
     [SerializeField] AudioClip clip;
     [SerializeField] AudioSource audioSource;
 
 
+    private void Update()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, Vector3.down, Color.red);
+
+        if (Physics.Raycast(ray, out hit, 1f, groundLayer))
+        {
+            LayerMask tempLayer = hit.transform.gameObject.layer;
+            switch (tempLayer.value)
+            {
+                case 16:
+                    clip = concreteClip;
+                    break;
+                case 17:
+                    clip = glassClip;
+                    break;
+                case 18:
+                    clip = grassClip;
+                    break;
+                case 19:
+                    clip = metalClip;
+                    break;
+                default:
+                    clip = concreteClip;
+                    break;
+            }
+        }
+    }
+
+
     public void PlayStepClip()
     {
+        print($"Playing footstep clip: {clip.name}");
         audioSource.Stop();
         audioSource.clip = clip;
         audioSource.Play();
