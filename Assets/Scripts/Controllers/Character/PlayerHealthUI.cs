5aecf171f0f19f723a61edf3b0984dbb9c088301
Henry Moss <henry@flip.fitness>
2023-08-06 15:55:57 -0400

    Imported new health ECG movie file; created a new render texture to display the movie file in the UI

--- Assets/Scripts/Controllers/Character/PlayerHealthUI.cs
+++ Assets/Scripts/Controllers/Character/PlayerHealthUI.cs
@@ -40,7 +40,7 @@ public class PlayerHealthUI : MonoBehaviour
         healthImage.color = healthColor;
 
         Rect rect = healthImage.uvRect;
-        rect.x += Time.deltaTime * -movementSpeed;
+        rect.x += Time.deltaTime * movementSpeed;
         healthImage.uvRect = rect;
 
         //if (PlayerController.instance.state == PlayerController.States.interacting)
