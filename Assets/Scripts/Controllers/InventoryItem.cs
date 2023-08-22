9a746cc40d53525b6ca7a49a426ef25f0c0aa43c
Henry Moss <henry@flip.fitness>
2023-08-22 11:35:38 -0400

    Populated inventory SpriteAtlas and updated inventoryItem prefab to refrence the atlas for assigning sprites; sprites in the atlas are called by the corresponing name to the item json; if a sprite is not located in the atlas then the prefab defaults to the placeholder sprite

--- Assets/Scripts/Controllers/InventoryItem.cs
+++ Assets/Scripts/Controllers/InventoryItem.cs
@@ -2,20 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.U2D;
 using TMPro;
 
 public class InventoryItem : MonoBehaviour
 {
     public int ID;
     public ItemInstance itemData;
+    [SerializeField] SpriteAtlas atlas;
     [SerializeField] Sprite icon;
     [SerializeField] Image image;
     [SerializeField] GameObject highlight;
 
 
+    private void Awake()
+    {
+        Sprite tempSprite = atlas.GetSprite(itemData.itemType.itemName);
+        if (tempSprite != null)
+        {
+            icon = tempSprite;
+            image.sprite = icon;
+        }
+    }
+
     public void DisplayItemData()
     {
-        InventoryController.instance.SelectItem(this);//.ShowItemData(itemData);
+        InventoryController.instance.SelectItem(this);
     }
 
     public void ToggleHighlight(bool value)
