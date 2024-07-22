fc6fa1e7e51c322e511fec68ca20de3157c2f3ea
Henry Moss <henry@flip.fitness>
2024-07-22 15:08:02 -0400

    Updated scene load order to be Intro > Radio Tower (removing the apartment level for now); udpated text strings for radio tower lore progression; created a new start room for the player to wake up in front of a television (currently does not save, only for visual interaction); adjusted scene start values so the player will wait for X seconds before standing up on first playthrough

--- Assets/Scripts/Menus/MainMenuController.cs
+++ Assets/Scripts/Menus/MainMenuController.cs
@@ -163,7 +163,7 @@ public class MainMenuController : MonoBehaviour
 
         SaveDataController.instance.CreateNewSaveFile();
         FadeController.instance.StartFade(1, 1.5f);
-        sceneToLoad = "Apartment";//"Intro"; 
+        sceneToLoad = "Intro"; 
         loadingScene = true;
     }
 
